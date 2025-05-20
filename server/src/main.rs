27e5f2d449d15885ea4b57c428f72affdefe1d0c
Mori <anders.morille@gmail.com>
2025-05-20 15:49:55 +0200

    Server starting

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 use axum::routing::get;
 use socketioxide::{
@@ -11,6 +14,9 @@ async fn on_connect(
     Data(data): Data<serde_json::Value>,
     State(state): State<AppState>,
 ) {
+    let state = state.state;
+
+    let socket_state = state.clone();
     socket.on(
         "event",
         async move |socket: SocketRef, Data::<serde_json::Value>(data)| {
@@ -28,7 +34,7 @@ async fn on_connect(
             };
 
             {
-                let player = &mut state.state.lock().unwrap().players[event.player_id];
+                let player = &mut socket_state.lock().unwrap().players[event.player_id];
 
                 match event.variant {
                     netcode::event::Variant::Jump(jump) => {
@@ -37,11 +43,25 @@ async fn on_connect(
                     netcode::event::Variant::Movement(movement) => player.x += movement,
                 };
             }
-            
-            let response = serde_json::to_string(state.state.as_ref()).unwrap();
+
+            let response = serde_json::to_string(socket_state.as_ref()).unwrap();
             socket.emit("state", &response).unwrap();
         },
     );
+
+    let global_state = state.clone();
+    tokio::spawn(async move {
+        let state = &global_state;
+        loop {
+            {
+                let state = state.lock().unwrap();
+                let message = serde_json::to_string(&*state).unwrap();
+                socket.emit("state", &message);
+            }
+            
+            tokio::time::sleep(Duration::from_millis(300)).await
+        }
+    });
 }
 
 #[derive(Debug, Clone, Default)]
@@ -51,9 +71,9 @@ struct AppState {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (layer, io) = SocketIo::builder()
-        .with_state(AppState::default())
-        .build_layer();
+    let state = AppState::default();
+
+    let (layer, io) = SocketIo::builder().with_state(state).build_layer();
     io.ns("/", on_connect);
 
     let app = axum::Router::new().layer(layer);
