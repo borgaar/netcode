4a0624d2f480a9d157d14dba7a4ea584e89a7543
Mori <anders.morille@gmail.com>
2025-05-20 12:38:24 +0200

    Compiling works for server

--- netcode/src/event.rs
+++ netcode/src/event.rs
@@ -1,16 +1,26 @@
 use std::time;
 
 use chrono::Utc;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub enum Event {
+    Jump(JumpEvent),
+    Movement(MovementEvent)
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct JumpEvent {
     at: chrono::DateTime<Utc>,
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct MovementEvent {
     direction: Direction,
     amount: f64,
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub enum Direction {
     Left,
     Right,
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use socketioxide::{
+    extract::{AckSender, Data, SocketRef},
+    SocketIo,
+};
+
+async fn on_connect(socket: SocketRef, Data(data): Data<serde_json::Value>) {
+    socket.emit("auth", &data).ok();
+
+    socket.on("message", async |socket: SocketRef, Data::<serde_json::Value>(data)| {
+        socket.emit("message-back", &data).ok();
+    });
+
+    socket.on(
+        "message-with-ack",
+        async |Data::<serde_json::Value>(data), ack: AckSender| {
+            ack.send(&data).ok();
+        },
+    );
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/", on_connect);
+
+    let app = axum::Router::new().layer(layer);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
