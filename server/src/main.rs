7aa7ab8ace805fdd69f4e4acd8833c30900230a2
Mori <anders.morille@gmail.com>
2025-05-22 16:22:25 +0200

    Remove clear ack on server

--- server/src/main.rs
+++ server/src/main.rs
@@ -87,7 +87,7 @@ async fn on_connect(socket: SocketRef, State(state): State<AppState>) {
 
                 // Ignore error since we can just wait for the next state broadcast
                 let _ = socket.emit(STATE_CHANNEL, &message);
-                state.clear_ack();
+                // state.clear_ack();
             }
 
             tokio::time::sleep(STATE_UPDATE_INTERVAL).await
