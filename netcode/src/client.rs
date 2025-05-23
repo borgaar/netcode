3c538bf005f52b0e27fddd83d55f7c5eccb1d07a
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-05-23 11:08:05 +0200

    added simulated ping to game struct

--- netcode/src/client.rs
+++ netcode/src/client.rs
@@ -1,6 +1,9 @@
 use std::{
     collections::HashMap,
-    sync::mpsc::{channel, Receiver},
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc, Mutex,
+    },
     thread, usize,
 };
 
@@ -14,8 +17,6 @@ use crate::{
     Action, State, ACTION_CHANNEL, ERROR_CHANNEL, JOIN_CHANNEL, STATE_CHANNEL,
 };
 
-const SIMULATED_PING_MS: u64 = 400;
-
 pub struct Game {
     state_receiver: Receiver<State>,
     join_receiver: Receiver<JoinResponse>,
@@ -25,6 +26,7 @@ pub struct Game {
     pub player_idx: Option<usize>,
     client: Client,
     unacknowledged: HashMap<Uuid, PlayerAction>,
+    simulated_ping: Arc<Mutex<u64>>,
 }
 
 impl Default for Game {
@@ -38,6 +40,8 @@ impl Game {
         let (state_sender, state_receiver) = channel::<State>();
         let (join_sender, join_receiver) = channel::<JoinResponse>();
 
+        let simulated_ping = Arc::new(Mutex::new(0));
+
         let game = Self {
             state_receiver,
             join_receiver,
@@ -46,61 +50,19 @@ impl Game {
             previous_state: Default::default(),
             target_state: Default::default(),
             player_idx: None,
-            client: ClientBuilder::new("http://localhost:7878")
-                .on(ERROR_CHANNEL, |payload, _| {
-                    let Payload::Text(val) = payload else {
-                        return;
-                    };
-                    eprintln!("{}", val.first().unwrap().as_str().unwrap());
-                })
-                .on(STATE_CHANNEL, move |payload, _| match payload {
-                    Payload::Text(text) => {
-                        let data = serde_json::from_str::<State>(
-                            text.first().unwrap().clone().as_str().unwrap(),
-                        )
-                        .unwrap();
-                        let sender = state_sender.clone();
-                        thread::spawn(move || {
-                            thread::sleep(std::time::Duration::from_millis(SIMULATED_PING_MS / 2));
-                            sender.send(data).unwrap();
-                        });
-                    }
-                    _ => {
-                        eprintln!("Received bad payload on state");
-                    }
-                })
-                .on(JOIN_CHANNEL, move |payload, _| match payload {
-                    Payload::Text(text) => {
-                        let data = serde_json::from_str::<JoinResponse>(
-                            text.first().unwrap().clone().as_str().unwrap(),
-                        )
-                        .unwrap();
-                        let sender = join_sender.clone();
-                        thread::spawn(move || {
-                            thread::sleep(std::time::Duration::from_millis(SIMULATED_PING_MS));
-                            sender.send(data).unwrap();
-                        });
-                    }
-                    _ => {
-                        eprintln!(
-                            "Received non-binary payload on join, received {:?}",
-                            payload
-                        );
-                    }
-                })
-                .connect()
-                .unwrap(),
+            client: build_netcode_client(state_sender, join_sender, simulated_ping.clone()),
+            simulated_ping,
         };
 
         game
     }
     pub fn join(&self) {
-        self.client
-            .emit(
-                ACTION_CHANNEL,
-                Payload::Text(vec![serde_json::to_value(&Action::Join).unwrap()]),
-            )
-            .unwrap();
+        if let Err(_) = self.client.emit(
+            ACTION_CHANNEL,
+            Payload::Text(vec![serde_json::to_value(&Action::Join).unwrap()]),
+        ) {
+            eprintln!("Failed to join the game");
+        };
     }
     pub fn update(&mut self) {
         self.state_update();
@@ -110,8 +72,8 @@ impl Game {
     fn calculate_interpolation_for_frame(&mut self) {
         let prev = self.previous_state.timestamp;
         let target = self.target_state.timestamp;
-        // ping / 2
-        let curr = Utc::now() - TimeDelta::milliseconds((SIMULATED_PING_MS / 2) as i64);
+        let curr =
+            Utc::now() - TimeDelta::milliseconds((*self.simulated_ping.lock().unwrap() / 2) as i64);
         let t = (curr - target).as_seconds_f64() / (target - prev).as_seconds_f64();
 
         let player_id = self.player_idx.unwrap_or(usize::MAX);
@@ -153,17 +115,20 @@ impl Game {
         self.display_state.players = new_curr_players;
     }
 
+    fn get_player(&self) -> Option<&Player> {
+        let player_id = self.player_idx?;
+        self.display_state.players.get(&player_id)
+    }
+
     fn state_update(&mut self) {
         for server_state in self.state_receiver.try_iter() {
             self.previous_state = self.target_state.clone();
             self.target_state = server_state.clone();
 
-            let Some(player_id) = self.player_idx else {
-                continue;
-            };
-
-            let Some(current_player) = self.display_state.players.get(&player_id) else {
-                continue;
+            // Get the current player
+            let current_player = match self.get_player() {
+                Some(player) => player.clone(),
+                None => continue,
             };
 
             // Print length of server play list
@@ -180,7 +145,7 @@ impl Game {
             let unacknowledged_x_diff = self.get_unack_x_diff();
 
             // Get the server's position for this player
-            let Some(server_player) = server_state.players.get(&player_id) else {
+            let Some(server_player) = server_state.players.get(&current_player.id) else {
                 continue;
             };
 
@@ -199,11 +164,11 @@ impl Game {
             self.display_state = server_state.clone();
 
             // Update the display position and jump time to the reconciled+predicted position
-            if let Some(player) = self.display_state.players.get_mut(&player_id) {
+            if let Some(player) = self.display_state.players.get_mut(&current_player.id) {
                 player.x = reconciled_position + position_discrepancy;
                 player.last_jump_at = Some(local_last_jump_at);
                 dbg!(player.x);
-                dbg!(server_state.players.get(&player_id).unwrap().x);
+                dbg!(server_state.players.get(&current_player.id).unwrap().x);
                 dbg!(reconciled_position);
                 dbg!(position_discrepancy);
                 println!("\n\n\n\n\n")
@@ -214,13 +179,14 @@ impl Game {
                 return;
             }
 
-            let action = Action::player_move(player_id, position_discrepancy);
+            let action = Action::player_move(current_player.id, position_discrepancy);
 
             // Add the action to the unacknowledged actions and send
             if let Some((id, player_action)) = action.ack_id() {
                 let client_clone = self.client.clone();
+                let ping = self.simulated_ping.clone();
                 thread::spawn(move || {
-                    thread::sleep(std::time::Duration::from_millis(SIMULATED_PING_MS / 2));
+                    thread::sleep(std::time::Duration::from_millis(*ping.lock().unwrap() / 2));
                     client_clone
                         .emit(
                             ACTION_CHANNEL,
@@ -271,8 +237,9 @@ impl Game {
                 .last_jump_at = Some(chrono::Utc::now());
 
             let client_clone = self.client.clone();
+            let ping = self.simulated_ping.clone();
             thread::spawn(move || {
-                thread::sleep(std::time::Duration::from_millis(SIMULATED_PING_MS / 2));
+                thread::sleep(std::time::Duration::from_millis(*ping.lock().unwrap() / 2));
                 client_clone
                     .emit(
                         ACTION_CHANNEL,
@@ -294,6 +261,66 @@ impl Game {
         // Optimistic update
         self.display_state.players.get_mut(&player_idx).unwrap().x += delta_x as f64;
     }
+
+    pub fn set_simulated_ping(&self, new_ping: u64) -> u64 {
+        let mut ping = self.simulated_ping.lock().unwrap();
+        *ping = new_ping;
+        *ping
+    }
+}
+
+fn build_netcode_client(
+    state_sender: Sender<State>,
+    join_sender: Sender<JoinResponse>,
+    ping: Arc<Mutex<u64>>,
+) -> Client {
+    let state_ping = ping.clone();
+    let join_ping = ping.clone();
+    ClientBuilder::new("http://localhost:7878")
+        .on(ERROR_CHANNEL, |payload, _| {
+            let Payload::Text(val) = payload else {
+                return;
+            };
+            eprintln!("{}", val.first().unwrap().as_str().unwrap());
+        })
+        .on(STATE_CHANNEL, move |payload, _| match payload {
+            Payload::Text(text) => {
+                let data =
+                    serde_json::from_str::<State>(text.first().unwrap().clone().as_str().unwrap())
+                        .unwrap();
+                let sender = state_sender.clone();
+                let ping = state_ping.clone();
+                thread::spawn(move || {
+                    thread::sleep(std::time::Duration::from_millis(*ping.lock().unwrap() / 2));
+                    sender.send(data).unwrap();
+                });
+            }
+            _ => {
+                eprintln!("Received bad payload on state");
+            }
+        })
+        .on(JOIN_CHANNEL, move |payload, _| match payload {
+            Payload::Text(text) => {
+                let data = serde_json::from_str::<JoinResponse>(
+                    text.first().unwrap().clone().as_str().unwrap(),
+                )
+                .unwrap();
+                let sender = join_sender.clone();
+                let ping = join_ping.clone();
+                thread::spawn(move || {
+                    thread::sleep(std::time::Duration::from_millis(*ping.lock().unwrap() / 2));
+                    sender.send(data).unwrap();
+                });
+            }
+            _ => {
+                eprintln!(
+                    "Received non-binary payload on join, received {:?}",
+                    payload
+                );
+            }
+        })
+        .connect()
+        .unwrap()
 }
 
 /// Linear interpolation between two values
